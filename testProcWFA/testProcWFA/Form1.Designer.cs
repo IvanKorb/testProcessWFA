51469db1fa3d03c6714449d730fa5b111be17ca1
IvanKorb <kor.ivan.1987@gmail.com>
2017-05-16 18:40:01 +0600

    Add files via upload

---
+++ testProcWFA/testProcWFA/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace testProcWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStart2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(486, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(128, 45);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStart2
+            // 
+            this.btnStart2.Location = new System.Drawing.Point(486, 138);
+            this.btnStart2.Name = "btnStart2";
+            this.btnStart2.Size = new System.Drawing.Size(128, 52);
+            this.btnStart2.TabIndex = 1;
+            this.btnStart2.Text = "Start 2";
+            this.btnStart2.UseVisualStyleBackColor = true;
+            this.btnStart2.Click += new System.EventHandler(this.btnStart2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 247);
+            this.Controls.Add(this.btnStart2);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStart2;
+    }
+}
+
---
+++ testProcWFA/testProcWFA/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace testProcWFA
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        public Form1()
+        {
+            //Process[] proc = Process.GetProcesses();
+
+            InitializeComponent();
+            Text = Thread.CurrentThread.Name + "/ id = " + Thread.CurrentThread.ManagedThreadId.ToString() + "/ background = "
+                + Thread.CurrentThread.IsBackground.ToString();
+            btnStart.Tag = 0;
+            thread1 = null;
+        }
+
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            if ((int)btnStart.Tag == 0)
+            {
+                GraphParam gParam = new GraphParam();
+                gParam.Canvas = CreateGraphics();
+                gParam.x = 10;
+                gParam.y = 10;
+                gParam.width = 200;
+                gParam.heith = 200;
+                thread1 = new Thread(ThreadGraphics);
+                thread1.IsBackground = true;
+                thread1.Start(gParam);
+                btnStart.Text = "Pause";
+                btnStart.Tag = 1;
+            }
+            else if ((int)btnStart.Tag == 1) {
+                thread1.Suspend();
+                btnStart.Text = "Continue";
+                btnStart.Tag = 2;
+            }
+            else
+            {
+                thread1.Resume();
+                btnStart.Text = "Pause";
+                btnStart.Tag = 1;
+            }
+               
+        }
+
+        private void btnStart2_Click(object sender, EventArgs e)
+        {
+            
+            GraphParam gParam = new GraphParam();
+            gParam.Canvas = CreateGraphics();
+            gParam.x = 10 + 250;
+            gParam.y = 10;
+            gParam.width = 200;
+            gParam.heith = 200;
+            Thread thread1 = new Thread(ThreadGraphics);
+            thread1.IsBackground = true;
+            thread1.Start(gParam);
+        }
+        void ThreadRoutine()
+        {
+            Application.Run(new Form1());
+        }
+        void ThreadGraphics(object param)
+        {
+            GraphParam gParam = (GraphParam)param;
+
+            gParam.Canvas.DrawRectangle(new Pen(Color.Black), gParam.x, gParam.y, gParam.width, gParam.heith);
+
+            Random rnd = new Random();
+            int x1, x2 = 0;
+            int y1, y2 = 0;
+
+            x1 = gParam.x;
+            y1 = rnd.Next(gParam.y, gParam.y + gParam.heith);
+            int iStep = 0;
+
+
+            while (true)
+            {
+                switch (iStep % 4)
+                {
+                    case 0:
+                        x2 = rnd.Next(gParam.x, gParam.x + gParam.width);
+                        y2 = gParam.y;
+                        break;
+                    case 1:
+                        x2 = gParam.x + gParam.width;
+                        y2 = rnd.Next(gParam.y, gParam.y + gParam.heith);
+                        break;
+                    case 2:
+                        x2 = rnd.Next(gParam.x, gParam.x + gParam.width);
+                        y2 = gParam.y + gParam.heith;
+                        break;
+                    case 3:
+                        x2 = gParam.x;
+                        y2 = rnd.Next(gParam.y, gParam.y + gParam.heith);
+                        break;
+
+
+                }
+
+                Color col = Color.FromArgb(
+                    rnd.Next(0, 255),
+                    rnd.Next(0, 255),
+                    rnd.Next(0, 255));
+
+                Pen pen = new Pen(col);
+                pen.Width = rnd.Next(1, 5);
+                gParam.Canvas.DrawLine(pen, x1, y1, x2, y2);
+                x1 = x2;
+                y1 = y2;
+
+                iStep++;
+                Thread.Sleep(10);
+                
+
+            }
+
+        }
+        class GraphParam
+        {
+            public Graphics Canvas;
+            public int x;
+            public int y;
+            public int width;
+            public int heith;
+
+        }
+
+
+    }
+}
